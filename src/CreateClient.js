2879aa06868bdf66dc5f1f3415030d42c30817c5
Soumya-0x000 <soumyadas429@gmail.com>
2024-06-26 12:57:53 +0530

    service role key is added, to improve control for admins

--- src/CreateClient.js
+++ src/CreateClient.js
@@ -2,5 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
 const apiKey = import.meta.env.VITE_SUPABASE_KEY;
+const serviceRoleKey = import.meta.env.VITE_SERVICE_ROLE_KEY;
 
 export const supabase = createClient(supabaseURL, apiKey);
+export const createServiceRoleClient = createClient(supabaseURL, serviceRoleKey);
